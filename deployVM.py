197f74e16d0fe7f5e6a9781ef93f47f2de28a20a
ӨᒪGΛ <58821195+0xRavel@users.noreply.github.com>
2023-03-27 18:42:33 -0300

    Made a separate function to create the vm config spec

--- deployVM.py
+++ deployVM.py
@@ -1,32 +1,44 @@
-import subprocess
 import argparse
+import subprocess
+from typing import Optional
+
 from pyVim.connect import SmartConnect, Disconnect
 from pyVmomi import vim
 import atexit
 
-def deploy_local(path, vmname):
-    # Run the VMWare Fusion to create & start a new VM
+
+def deploy_local(path: str, vmname: str) -> None:
     subprocess.run(['vmrun', 'clone', path, vmname])
     subprocess.run(['vmrun', 'start', vmname])
     print(f"Deploying a VM from {path} with name {vmname}")
 
-def deploy_remote(host, user, pwd):
-    # Connect to vCenter
+
+def deploy_remote(host: str, user: str, pwd: str) -> None:
     si = SmartConnect(host=host, user=user, pwd=pwd)
     atexit.register(Disconnect, si)
     content = si.RetrieveContent()
     root_folder = content.rootFolder
     resource_pool = root_folder.childEntity[0].resourcePool
     datastore = root_folder.childEntity[0].datastore[0]
-    # Create a new vm config spec
-    vm_config = vim.vm.ConfigSpec(
+
+    vm_config = create_vm_config_spec(datastore)
+
+    root_folder.CreateVm(config=vm_config, pool=resource_pool)
+    new_vm = root_folder.childEntity[0].vm[0]
+    new_vm.PowerOn()
+    print(f"New virtual machine '{new_vm.name}' has been deployed and powered on.")
+    si.logout()
+
+
+def create_vm_config_spec(datastore: vim.Datastore) -> vim.vm.ConfigSpec:
+    return vim.vm.ConfigSpec(
         name='new_vm',
         memoryMB=2048,
         numCPUs=2,
         files=vim.vm.FileInfo(logDirectory=None,
                                snapshotDirectory=None,
                                suspendDirectory=None,
-                               vmPathName="[" + datastore.name + "] new_vm"),
+                               vmPathName=f"[{datastore.name}] new_vm"),
         deviceChange=[
             vim.vm.device.VirtualDeviceSpec(
                 operation='add',
@@ -40,35 +52,29 @@ def deploy_remote(host, user, pwd):
             )
         ]
     )
-    # Create & power on the new VM
-    root_folder.CreateVm(config=vm_config, pool=resource_pool)
-    new_vm = root_folder.childEntity[0].vm[0]
-    new_vm.PowerOn()
-    print("New virtual machine '{}' has been deployed and powered on.".format(new_vm.name))
-    si.logout()
 
-def main():
+
+def main() -> None:
     parser = argparse.ArgumentParser(description='Deploy a VM')
-    parser.add_argument('--local', action='store_true', help='Deploy a VM locally')
-    parser.add_argument('--remote', action='store_true', help='Deploy a VM remotely')
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--local', action='store_true', help='Deploy a VM locally')
+    group.add_argument('--remote', action='store_true', help='Deploy a VM remotely')
     parser.add_argument('--path', type=str, help='Path to the VM image')
-    parser.add_argument('--vmname', type=str, help='Name of the new VM') 
+    parser.add_argument('--vmname', type=str, help='Name of the new VM')
     parser.add_argument('--host', type=str, help='Vcenter hostname')
     parser.add_argument('--user', type=str, help='Username')
     parser.add_argument('--pwd', type=str, help='Password')
     args = parser.parse_args()
-    if args.local and args.remote:
-        print("Please select only one option")
+
     if args.local:
+        if args.path is None or args.vmname is None:
+            parser.error("--local option requires --path and --vmname options")
         deploy_local(args.path, args.vmname)
     elif args.remote:
+        if args.host is None or args.user is None or args.pwd is None:
+            parser.error("--remote option requires --host, --user, and --pwd options")
         deploy_remote(args.host, args.user, args.pwd)
-    if args.local and (args.path is None or args.vmname is None):
-        parser.error("--local option requires --path and --vmname options")
-    elif args.remote and (args.host is None or args.user is None or args.pwd is None):
-        parser.error("--remote option requires --host, --user, and --pwd options")
-    else:
-        print("Use -h to see the options")
 
-if __name__=="__main__": 
-	main()
+
+if __name__ == "__main__":
+    main()
