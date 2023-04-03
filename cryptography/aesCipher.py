735bf035096cb074c2e7b08b64551ef461e458f5
ӨᒪGΛ <58821195+0xRavel@users.noreply.github.com>
2023-04-02 22:59:32 -0300

    Update aesCipher.py

--- cryptography/aesCipher.py
+++ cryptography/aesCipher.py
@@ -1,56 +1,61 @@
-import os
 import base64
 import errno
+import os
 from Crypto.Cipher import AES
+from typing import List
 
 class AESCipher:
-    def __init__(self, key, iv):
+    def __init__(self, key: str, iv: str):
         self.key = key
         self.iv = iv
 
-    def encrypt(self, source):
+    def encrypt(self, source: str) -> str:
         cipher = AES.new(self.key, AES.MODE_CBC, self.iv)
-        padding = AES.block_size - len(source) % AES.block_size 
+        padding = AES.block_size - len(source) % AES.block_size
         data = source + chr(padding) * padding
-        return base64.b64encode(cipher.encrypt(data))
+        return base64.b64encode(cipher.encrypt(data)).decode()
 
-    def decrypt(self, source):
+    def decrypt(self, source: str) -> str:
         cipher = AES.new(self.key, AES.MODE_CBC, self.iv)
         data = cipher.decrypt(base64.b64decode(source))
         padding = ord(data[-1])
         if data[-padding:] != chr(padding) * padding:
             raise ValueError("Invalid padding...")
-        return data[:-padding]
+        return data[:-padding].decode()
 
-def makedir(directory):
+def makedir(directory: str) -> None:
     try:
         os.makedirs(directory)
     except OSError as e:
         if e.errno != errno.EEXIST:
             print("[E] makedir")
 
-action = input("Enter 'enc' to encrypt or 'dec' to decrypt: ")
-key = input("Enter secret key: ")
-iv = input("Enter iv value: ")
-files = input("Enter the file(s) you want to encrypt/decrypt: ")
+def process_files(action: str, files: List[str], cipher: AESCipher) -> None:
+    makedir(action)
 
-cipher = AESCipher(key, iv)
+    for filename in files:
+        out_path = os.path.join(action, filename)
+        with open(filename, 'r') as inf:
+            data = inf.read()
 
-makedir(action)
+        if action == "dec":
+            data = data.replace('\n', '')
+            decrypted_data = cipher.decrypt(data)
+            with open(out_path, "w") as f:
+                f.write(decrypted_data)
+            print(f"Decrypted data have been written into '{out_path}'")
+        elif action == "enc":
+            encrypted_data = cipher.encrypt(data)
+            with open(out_path, "w") as f:
+                f.write(encrypted_data)
+            print(f"Encrypted data have been written into '{out_path}'")
 
-if type(files) == str:
-    files = [files]
-    
-for filename in files:
-    out_path = action + "/" + filename
-    with open(filename, 'r') as inf:
-        data = inf.read()
-    if action == "dec":
-        data = data.replace('\n', '')
-        with open(out_path, "w") as f:
-            f.write(cipher.decrypt(data))
-        print("Decrypted data have been written into '" + out_path + "'")
-    elif action == "enc":
-        with open(out_path, "w") as f:
-            f.write(cipher.encrypt(data))
-        print("Encrypted data have been written into '" + out_path + "'")
+if __name__ == '__main__':
+    action = input("Enter 'enc' to encrypt or 'dec' to decrypt: ")
+    key = input("Enter secret key: ")
+    iv = input("Enter iv value: ")
+    files_input = input("Enter the file(s) you want to encrypt/decrypt (separated by commas): ")
+
+    files = [file.strip() for file in files_input.split(',')]
+    cipher = AESCipher(key, iv)
+    process_files(action, files, cipher)
